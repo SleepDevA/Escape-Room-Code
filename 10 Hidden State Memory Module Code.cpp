e8dd2a5f8abb2c412b71e4138fb6ebb07e553ade
SleepDevA <Copernicus998@gmail.com>
2026-03-12 09:35:50 -0500

    Added more comments
    Updated potentiometer reading delay and added comments for clarity.

--- 10 Hidden State Memory Module Code.cpp
+++ 10 Hidden State Memory Module Code.cpp
@@ -1,10 +1,10 @@
-const int b1 = 2;
+const int b1 = 2; // Defining Buttons
 const int b2 = 3;
 const int b3 = 9;
 const int pot = A1;
 const int led = 8;
 int potValue1 = 0;
-int stage = 0;
+int stage = 0; // Ensure the buttons are pressed in the right order for completion
 
 void setup() {
     Serial.begin(9600);
@@ -17,12 +17,12 @@ void setup() {
 
 void loop() {
     int potValue = analogRead(pot);
-    potValue1 = map(potValue, 0, 1023, 0, 100); // map to percentage for easier debugging
+    potValue1 = map(potValue, 0, 1023, 0, 100); // map to percentage for easier debugging can be changed in the future
     Serial.print("Potentiometer value: ");
     Serial.println(potValue1);
-    delay(100); // small delay to avoid flooding the serial output
+    delay(500); // Decreases Printing to serial
 
-    bool potOK = (potValue >= 200 && potValue <= 400);
+    bool potOK = (potValue >= 200 && potValue <= 400); // bool defining pot range
 
     // only move to stage 1 once and when starting from stage 0
     if (stage == 0 && potOK) {
@@ -45,7 +45,8 @@ void loop() {
     }
 
     if (stage == 4) {
-        Serial.println("Completed");
+        Serial.println("Completed");   // current completion function
         digitalWrite(led, HIGH);
     }
 }
+
